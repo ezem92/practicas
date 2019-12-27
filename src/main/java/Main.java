a19860ce6c386df231478c088162a37070d6c4c8
tiverow <eze.montivero@live.com>
2019-12-26 22:01:16 -0300

    pa k lo veas jaja salu2

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+
+        Empleado empleado = new Empleado("Eze", 10, true);
+        System.out.println(empleado.isActive());
+
+        empleado.setActive(false);
+        System.out.println(empleado.isActive());
+
+        Farm farm = new Farm();
+        farm.getAnimals().add(new Perro());
+        if (farm.getAnimals().isEmpty()) {
+            System.out.println("no hay animales");
+        } else {
+            System.out.println("hay animales");
+
+        }
+        farm.addEmpleado(empleado);
+
+        for (Person person : farm.getPersons()) {
+            System.out.println(person.getName());
+        }
+    }
+}
